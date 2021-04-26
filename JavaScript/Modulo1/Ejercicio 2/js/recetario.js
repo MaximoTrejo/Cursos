5706563dbd6780271005f2ef619ddebeeeb9a177
MaximoTrejo <78154751+MaximoTrejo@users.noreply.github.com>
2021-04-26 10:39:05 -1100

    JavaScript
    JavaScript

---
+++ JavaScript/Modulo1/Ejercicio 2/js/recetario.js
@@ -0,0 +1,3 @@
+var primerIngrediente = prompt('Ingrese el primer ingrediente')
+var segundoIngrediente =prompt('Ingrese el segundo ingrediente')
+var tercerIngrediente = prompt('Ingrese el tercer ingrediente')
---
+++ JavaScript/Modulo1/Ejercicio 4/evaluacion 4/js/js.js
@@ -0,0 +1,18 @@
+var body =document.getElementsByTagName('body')
+body[0].style.backgroundColor ='#fcf79f'
+
+var destacado = document.getElementById('destacado')
+destacado.style.fontSize='24px'
+
+var titulos =document.getElementsByTagName('h2')
+titulos[0].style.fontFamily ='Arial'
+titulos[0].style.fontFamily ='Arial'
+
+var parrafos =document.getElementsByTagName('p')
+parrafos[0].style.color='#0ca001'
+parrafos[1].style.color='#0ca001'
+parrafos[2].style.color='#0ca001'
+parrafos[4].style.color='#0ca001'
+
+var vinculo = document.getElementById('vinculo')
+vinculo.innerHTML ='<a href="https://www.lipsum.com">Limsum</a>'
